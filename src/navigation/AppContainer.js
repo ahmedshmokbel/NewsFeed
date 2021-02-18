2dfa0df11b77d8776530b93af473e38164d4c661
ahmed mokbel <amokbel@2p.com.sa>
2021-02-19 00:05:27 +0200

    check internet

--- src/navigation/AppContainer.js
+++ src/navigation/AppContainer.js
@@ -1,13 +1,26 @@
 import React, { useState, useEffect, useMemo, useRef, useReducer } from 'react';
 import NetInfo from "@react-native-community/netinfo";
- import { NavigationContainer } from "@react-navigation/native";
- 
+import { NavigationContainer } from "@react-navigation/native";
+import NoInternetHeader from '../Components/NoInternetHeader'
 
 const AppContainer = () => {
- 
+    //state 
+    const [online, setOnline] = useState(true);
+
+
+    useEffect(() => {
+        // Subscribe for connection status
+        const unsubscribe = NetInfo.addEventListener(state => {
+            dispatch(updateConnectionStatus(state.isConnected));
+            setOnline(state.isConnected);
+        });
+        return () => {
+            unsubscribe();
+        };
+    }, [online])
     return (
         <NavigationContainer>
-            
+            {online ? null : <NoInternetHeader />}
         </NavigationContainer >
     )
 }
