44b5ad1cde1f18da3929161dfcd02c9411398a0f
ahmed mokbel <amokbel@2p.com.sa>
2021-02-18 23:53:07 +0200

    adding localization

---
+++ src/Localization/Translate.js
@@ -0,0 +1,25 @@
+import i18n from 'i18n-js';
+
+// i18n.locale = I18n.currentLocale();
+
+export default tran = i18n.translations = {
+    en: {
+        //Errors
+        noInternet: 'No internet connection',
+       
+        Search: 'Search',
+
+
+
+       
+ 
+
+    },
+    ar: {
+        //Errors
+        noInternet: 'لا يوجد اتصال بالانترنت',
+       Search :'بحث'
+    },
+
+}
+
