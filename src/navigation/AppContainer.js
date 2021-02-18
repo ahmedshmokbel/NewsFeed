6ef2802de7449105e71921f280575214cca0eac8
ahmed mokbel <amokbel@2p.com.sa>
2021-02-18 23:58:29 +0200

    creating app container navigation

---
+++ src/navigation/AppContainer.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect, useMemo, useRef, useReducer } from 'react';
+import NetInfo from "@react-native-community/netinfo";
+ import { NavigationContainer } from "@react-navigation/native";
+ 
+
+const AppContainer = () => {
+ 
+    return (
+        <NavigationContainer>
+            
+        </NavigationContainer >
+    )
+}
+
+
+export default AppContainer
