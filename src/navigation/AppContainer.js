ea78a97cb555a7678ff2a4ca2d50b8b8cfc2f5c1
ahmed mokbel <amokbel@2p.com.sa>
2021-02-19 00:06:40 +0200

    adding localization to container

--- src/navigation/AppContainer.js
+++ src/navigation/AppContainer.js
@@ -2,11 +2,29 @@ import React, { useState, useEffect, useMemo, useRef, useReducer } from 'react';
 import NetInfo from "@react-native-community/netinfo";
 import { NavigationContainer } from "@react-navigation/native";
 import NoInternetHeader from '../Components/NoInternetHeader'
+import i18n from 'i18n-js';
+import { I18nManager } from 'react-native';
 
 const AppContainer = () => {
     //state 
     const [online, setOnline] = useState(true);
 
+    useEffect(() => {
+         
+        i18n.locale = SettingsState.Lang
+
+
+        if (SettingsState.Lang == 'en') {
+
+            I18nManager.allowRTL(false);
+            I18nManager.forceRTL(false);
+        } else {
+            I18nManager.allowRTL(true);
+            I18nManager.forceRTL(true);
+        }
+
+
+    })
 
     useEffect(() => {
         // Subscribe for connection status
